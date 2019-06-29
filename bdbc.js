08f37de6dc59d43c658ddb0e4e0e0daa2d9c4746
mile_leite <jamilkshake@gmail.com>
2019-06-29 10:49:21 -0300

    Banco de dados da biblioteca para JSON

---
+++ bdbc.js
@@ -0,0 +1,22 @@
+var url = "rosesc_2019-06-05-9-28-43.xlsx";
+
+/* set up async GET request */
+var req = new XMLHttpRequest();
+req.open("GET", url, true);
+req.responseType = "arraybuffer";
+
+req.onload = function(e) {
+  var data = new Uint8Array(req.response);
+  var workbook = XLSX.read(data, {type:"array"});
+
+  /* DO SOMETHING WITH workbook HERE */
+
+  var first_sheet_name = workbook.SheetNames[0];
+
+    /* Get worksheet */
+    var worksheet = workbook.Sheets[first_sheet_name];
+
+    var bd = XLSX.utils.sheet_to_json(worksheet));
+}
+
+req.send();
